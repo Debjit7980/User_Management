27691be605de1ce64dbdbe380f1282b4cb962afe
Debjit7980 <62738111+Debjit7980@users.noreply.github.com>
2024-01-15 18:30:58 +0530

    Update Delete.js

--- login-signup-app/src/Components/Delete.js
+++ login-signup-app/src/Components/Delete.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function Delete() {
   return (
-    <div className='container mx-auto p-2 mt-6  text-center shadow bg-dark'>
+    <div className='container mx-auto p-2 mt-6  text-center shadow bg-dark delete-page'>
         <h5 className='text-light mx-auto'>The record has been deleted Successfully</h5>
         <Link to="/" className='btn btn-success mt-2 w-100 mx-auto'>Back</Link>
     </div>
